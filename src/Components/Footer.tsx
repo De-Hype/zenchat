d75fd60f0923b01433a6b02e3d95f2c9d8bcba8a
De-Hype <iamsocialhype@gmail.com>
2024-03-01 03:24:22 -0800

    Added some components

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,11 @@
+
+type Props ={};
+const Footer = (props:Props) => {
+  return (
+    <div className="">
+
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/Components/NonUserHeader.tsx
@@ -0,0 +1,12 @@
+
+type Props = {}
+const NotUserHeader = (props: Props) => {
+    
+  return (
+    <div className="">
+        
+    </div>
+  )
+}
+
+export default NotUserHeader;
