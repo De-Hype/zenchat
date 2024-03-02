3c5013fd3e42b15654d5fcd42aca01d99c11df7c
De-Hype <iamsocialhype@gmail.com>
2024-03-02 03:30:24 -0800

    Started work on the header component

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,8 +1,15 @@
-
+import { FaUserFriends } from "react-icons/fa";
+import { SiZendesk } from "react-icons/si";
+import {IoMdNotifications, IoMdSettings } from "react-icons/io";
 const Header = () => {
   return (
-    <div className="">
-        
+    <div className="flex justify-between items-center">
+        <SiZendesk className="text-xl" />
+        <div className="">
+          <FaUserFriends className="" />
+          <IoMdNotifications className="" />
+          <IoMdSettings className="" />
+        </div>
     </div>
   )
 }
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useState } from "react"
 
 type Props = {}
@@ -5,19 +6,21 @@ const page = (props: Props) => {
     const [name, setName] = useState('');
     const [username, setUserName] = useState('');
     const [password, setPassword] = useState('');
-    const HandleRegister=(e):any =>{
+    const HandleRegister=(e:any) =>{
         e.preventDefault()
     }
   return (
     <div className="min-h-screen">
-        <form className="">
+      <div className="">
+        <form onSubmit={HandleRegister} className="">
             <input type="text" name="name" placeholder="Name" id="" />
             <input type="text" name="username" placeholder="Username" id="" />
             <input type="password" name="password" placeholder="Password" id="" />
             <input type="submit" value="Register" onClick={(e)=>HandleRegister(e)} />
         </form>
-        <button type="button">Google</button>
-        <button type="button">Facebook</button>
+        <button className="" type="button">Register with Google</button>
+        <button className="" type="button">Register with Github</button>
+      </div>
     </div>
   )
 }
